ae3e717a6192066b3984d02f665d5734b6258c18
quangson91 <quangson91@gmail.com>
2020-07-26 10:51:03 +0700

    Remove unused files

--- src/main/java/uos/dev/restcli/parser/grammar/GrammarParserException.kt
+++
@@ -1,8 +0,0 @@
-package uos.dev.restcli.parser.grammar
-
-import java.lang.RuntimeException
-
-class GrammarParserException(
-    clazz: Class<*>,
-    input: String
-) : RuntimeException("Parse grammar ${clazz.simpleName} error for: $input")
