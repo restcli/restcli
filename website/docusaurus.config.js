aecc430b98be771fbcfc6313a811e2e5fe7b9a33
quangson91 <quangson91@gmail.com>
2020-08-27 06:30:39 +0700

    Update side config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'restcli',
+  tagline: 'A missing commandline application for execute IntelliJ HTTP Client files',
+  url: 'https://quangson91.github.io/intellij_rest_cli/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'quangson91', // Usually your GitHub org/user name.
+  projectName: 'intellij_rest_cli', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'restcli',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'rest-cli-logo',
+        src: 'img/logo.png',
       },
       items: [
         {
@@ -21,9 +21,8 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/quangson91/intellij_rest_cli',
           label: 'GitHub',
           position: 'right',
         },
@@ -31,52 +30,7 @@ module.exports = {
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} QuangSon. Built with Docusaurus.`,
     },
   },
   presets: [
@@ -89,13 +43,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/quangson91/intellij_rest_cli/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
