aed490a0a2d1a55588182001182596d06e5d45ec
quangson91 <quangson91@gmail.com>
2020-09-18 23:18:11 +0700

    Update package script

--- brew/package.sh
+++ brew/package.sh
@@ -13,10 +13,9 @@ cp "brew/restcli" "$PACKAGE_DIRECTORY/brew"
 jar_file=$(ls -1 build/libs/*.jar | head -1)
 cp "$jar_file" "$PACKAGE_DIRECTORY/restcli.jar"
 
-# shellcheck disable=SC2164
-pushd $PACKAGE_DIRECTORY_ROOT
-zip -r brew_restcli.zip "brew_restcli"
-zip -j restcli.zip "brew_restcli/restcli.jar"
-shasum -a 256 brew_restcli.zip restcli.zip > checksums.txt
-# shellcheck disable=SC2164
-popd
+(
+  cd $PACKAGE_DIRECTORY_ROOT &&
+    zip -r brew_restcli.zip "brew_restcli" &&
+    zip -j restcli.zip "brew_restcli/restcli.jar" &&
+    shasum -a 256 brew_restcli.zip restcli.zip >checksums.txt
+)
