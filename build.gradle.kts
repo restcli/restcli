8dcdc30d5fdade75714173c7f4350514d53b1d58
quangson91 <quangson91@gmail.com>
2021-12-29 10:22:04 +0700

    Upgrade deps & add test for check share variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlinVersion = "1.6.0"
+    val kotlinVersion = "1.6.10"
     java
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
@@ -31,24 +31,24 @@ publishing {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    val junitVersion = "5.6.0"
+    val junitVersion = "5.8.2"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    implementation("info.picocli:picocli:4.3.2")
-    kapt("info.picocli:picocli-codegen:4.3.2")
-    testImplementation("com.google.truth:truth:1.0.1")
-    implementation("com.squareup.okhttp3:okhttp:4.7.2")
-    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("info.picocli:picocli:4.6.2")
+    kapt("info.picocli:picocli-codegen:4.6.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.google.code.gson:gson:2.8.9")
     implementation("org.apache.commons:commons-text:1.9")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.8.0")
-    implementation("commons-validator:commons-validator:1.6")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("commons-validator:commons-validator:1.7")
     implementation("com.jakewharton.picnic:picnic:0.5.0")
     implementation("com.github.ajalt:mordant:1.2.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
-    implementation("io.github.microutils:kotlin-logging:1.8.3")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.3")
-    val graalvmVersion = "21.1.0"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
+    val graalvmVersion = "21.2.0"
     implementation("org.graalvm.js:js:$graalvmVersion") { exclude("org.graalvm.regex") }
     implementation("org.graalvm.js:js-scriptengine:$graalvmVersion")
 }
--- src/test/java/uos/dev/restcli/E2ETest.kt
+++ src/test/java/uos/dev/restcli/E2ETest.kt
@@ -1,6 +1,7 @@
 package uos.dev.restcli
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import uos.dev.restcli.report.TestReportStore
@@ -8,18 +9,12 @@ import uos.dev.restcli.report.TestReportStore
 class E2ETest {
     /**
      * Tests execute the http request `fileName` in the test resource folder.
-     * If you running the test on the IDE, you have to set the working directory point to the test
+     * If you are running the test on the IDE, you have to set the working directory point to the test
      * resource folder.
      */
     @ParameterizedTest
     @CsvSource(
-        value = [
-            "get-requests.http",
-            "post-requests.http",
-            "requests-with-authorization.http",
-            "requests-with-name.http",
-            "requests-with-tests.http"
-        ]
+        value = ["get-requests.http", "post-requests.http", "requests-with-authorization.http", "requests-with-name.http", "requests-with-tests.http"]
     )
     fun `should not fail requests`(fileName: String) {
         // Given
@@ -42,9 +37,7 @@ class E2ETest {
 
     @ParameterizedTest
     @CsvSource(
-        value = [
-            "requests-bad-ssl.http"
-        ]
+        value = ["requests-bad-ssl.http"]
     )
     fun `request with bad ssl`(fileName: String) {
         // Given
@@ -65,9 +58,7 @@ class E2ETest {
 
     @ParameterizedTest
     @CsvSource(
-        value = [
-            "requests-with-failing-tests.http"
-        ]
+        value = ["requests-with-failing-tests.http"]
     )
     fun `should fail request`(fileName: String) {
         // Given
@@ -86,5 +77,30 @@ class E2ETest {
         assertThat(exitCode).isEqualTo(1)
 
         assertThat(TestReportStore.testGroupReports.all { it.testReports.size > 0 }).isTrue()
+    }
+
+    @Test
+    fun `should share variables between two request files`() {
+        val paths = arrayOf(
+            "requests-share-var-between-files1.http",
+            "requests-share-var-between-files2.http",
+        )
+
+        // Given
+        val httpFilePaths = paths.map { javaClass.getResource("/requests/$it").path }.toTypedArray()
+        val restCli = RestCli().apply {
+            environmentName = "test"
+            logLevel = HttpLoggingLevel.BASIC
+            this.httpFilePaths = httpFilePaths
+            environmentFilesDirectory = javaClass.getResource("/requests/").path
+        }
+
+        // When
+        val exitCode = restCli.call()
+
+        // Then
+        assertThat(exitCode).isEqualTo(1)
+
+        assertThat(TestReportStore.testGroupReports.all { it.testReports.isNotEmpty() }).isTrue()
     }
 }
